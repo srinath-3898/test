3f0842857087eeb7a7b1d34dcb556d7942f8e5d2
srinath-3898 <srinath3898@gmail.com>
2023-07-29 12:33:44 +0530

    DOM manipulatiions task 3

---
+++ dom.js
@@ -0,0 +1,20 @@
+// console.dir(document);
+// console.log(document.URL);
+// console.log(document.title);
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.forms);
+// console.log(document.images);
+
+//GET ELEMENT BY ID
+
+let headerTitle = document.getElementById("header-title");
+let header = document.getElementById("main-header");
+console.log(headerTitle);
+// console.log(headerTitle.textContent);
+// console.log(headerTitle.innerText);
+// headerTitle.textContent = "Hello";
+// headerTitle.innerText = "Good bye";
+// headerTitle.innerHTML = "<h3>Hello</h3>";
+header.style.borderBottom = "3px solid #000";
