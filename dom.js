94db4c0f0df48201b2fcd1118e17ece953dda5c8
srinath-3898 <srinath3898@gmail.com>
2023-07-29 13:18:50 +0530

    Dom manipulation task 4

--- dom.js
+++ dom.js
@@ -9,12 +9,14 @@
 
 //GET ELEMENT BY ID
 
-let headerTitle = document.getElementById("header-title");
-let header = document.getElementById("main-header");
-console.log(headerTitle);
-// console.log(headerTitle.textContent);
-// console.log(headerTitle.innerText);
-// headerTitle.textContent = "Hello";
-// headerTitle.innerText = "Good bye";
-// headerTitle.innerHTML = "<h3>Hello</h3>";
-header.style.borderBottom = "3px solid #000";
+// let headerTitle = document.getElementById("header-title");
+// let header = document.getElementById("main-header");
+// console.log(headerTitle);
+// // console.log(headerTitle.textContent);
+// // console.log(headerTitle.innerText);
+// // headerTitle.textContent = "Hello";
+// // headerTitle.innerText = "Good bye";
+// // headerTitle.innerHTML = "<h3>Hello</h3>";
+// header.style.borderBottom = "3px solid #000";
+
+let items = document.getElementsByClassName("list-group");
