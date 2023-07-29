f2e5108f2ef24c2fd9372755c8b8d3233829bffc
srinath-3898 <srinath3898@gmail.com>
2023-07-29 14:44:39 +0530

    Dom manipulation task-7

--- dom.js
+++ dom.js
@@ -19,17 +19,29 @@
 // // headerTitle.innerHTML = "<h3>Hello</h3>";
 // header.style.borderBottom = "3px solid #000";
 
-const itemList = document.getElementById("items");
+// const itemList = document.getElementById("items");
 
-const listItems = itemList.querySelectorAll("li");
+// const listItems = itemList.querySelectorAll("li");
 
-for (const li of listItems) {
-  li.style.backgroundColor = "lightblue";
-}
-listItems[1].style.backgroundColor = "green";
-listItems[2].style.display = "none";
+// for (const li of listItems) {
+//   li.style.backgroundColor = "lightblue";
+// }
+// listItems[1].style.backgroundColor = "green";
+// listItems[2].style.display = "none";
 
-const newListItem = document.createElement("li");
-newListItem.textContent = "New Item";
+// const newListItem = document.createElement("li");
+// newListItem.textContent = "New Item";
 
-itemList.appendChild(newListItem);
+// itemList.appendChild(newListItem);
+// let items = document.getElementsByClassName("list-group");
+// for (let i = 0; i < items[0].children.length; i++) {
+//   items[0].children[i].style.backgroundColor = "white";
+// }
+
+var itemList = document.querySelector("#items");
+// itemList.parentElement.style.backgroundColor = "#f4f4f4";
+// console.log(itemList.parentElement.parentElement.parentElement);
+// let lastChild = itemList.lastElementChild;
+// lastChild.textContent = "Hello last child";
+console.log(itemList.nextElementSibling);
+console.log(itemList.previousElementSibling);
